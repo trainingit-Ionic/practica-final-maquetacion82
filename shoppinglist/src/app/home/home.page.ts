d62699ad5466c943a870f9e254b105ff09346b0f
maquetacion82 <eugenia.gil@altia.es>
2019-11-07 12:22:20 +0100

    Cambios

--- shoppinglist/src/app/home/home.page.ts
+++ shoppinglist/src/app/home/home.page.ts
@@ -1,5 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { Component } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
 interface Itemtoken {
@@ -29,16 +28,37 @@ export class HomePage {
         if (v.isDeleted === false) {
           this.mylist.push(v);
         }
-        this.history.push(v.title);
-        console.log(this.history);
+        if ((this.history.length) === 0) {
+          this.history.push(v.title);
+        } else {
+          if (this.history.indexOf(v.title) === -1) {
+            this.history.push(v.title);
+          }
+        }
+        //console.log(this.history);
       });
     });
   }
 
+  clearAll() {
+    this.storage.ready().then(() => {
+      this.storage.forEach((v, k) => {
+        if (v.isDeleted === false) {
+          v.isDeleted = true;
+          this.storage.set(k, v);
+        }
+        this.history.push(v.title);
+        //console.log(this.history);
+      });
+    });
+    this.mylist = [];
+
+  }
+
   search(event) {
-    console.log(event);
+    //console.log(event);
     this.searhHistory = this.history;
-    console.log(this.searhHistory);
+    //console.log(this.searhHistory);
     // set val to the value of the searchbar
     const val = event.target.value;
     // if the value is an empty string don't filter the items
@@ -66,8 +86,10 @@ export class HomePage {
     this.itemtoken.isDeleted = false;
     //Añadimos item al array
     this.mylist.push(this.itemtoken);
-    this.history.push(this.itemtoken.title);
-    console.log(this.history);
+    if (this.history.indexOf(this.itemtoken.title) === -1) {
+      this.history.push(this.itemtoken.title);
+    //console.log(this.history);
+    }
     this.inputValue = '';
     this.storage.ready().then(() => {
       this.storage.set(this.itemtoken.id, this.itemtoken);
@@ -87,7 +109,7 @@ export class HomePage {
   }
 
   modifyValue(e): void {
-    console.log(!e.isChecked);
+    //console.log(!e.isChecked);
     this.itemtoken = {id: '', title: '', isChecked: false, isDeleted: false};
     this.storage.get(e.id).then(
       (item) => {
@@ -96,14 +118,14 @@ export class HomePage {
           this.itemtoken.title = item.title;
           this.itemtoken.isChecked = e.isChecked;
           this.itemtoken.isDeleted = item.isDeleted;
-          console.log('item' + this.itemtoken);
+          //console.log('item' + this.itemtoken);
           this.setStorage(this.itemtoken);
         }
       });
   }
 
   setStorage(item): void {
-    console.log('set'+ item.id);
+    //console.log('set'+ item.id);
     this.storage.ready().then(() => {
       this.storage.set(item.id, item);
     });
